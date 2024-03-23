8c7467bc4ec2e8053f8360384f36a64553c4fd24
jesusre890 <jesusre890@gmail.com>
2024-03-23 19:39:54 -0300

    new api

--- src/components/blogComponents/Cards.jsx
+++ src/components/blogComponents/Cards.jsx
@@ -11,13 +11,20 @@ import {useEffect,useState} from "react";
 const Cards=() => {
   const [apiData,setApiData]=useState([]);
   
+  //useEffect(() => {
+  //  fetch(import.meta.env.VITE_APP_API)
+  //    .then((response) => response.json())
+  //    .then((data) => setApiData(data.articles))
+  //    .catch((error) => console.error("Error fetching data:", error));
+  //},[]);
+
   useEffect(() => {
-    fetch(import.meta.env.VITE_APP_API)
+    fetch("https://jsonplaceholder.typicode.com/photos")
       .then((response) => response.json())
-      .then((data) => setApiData(data.articles))
+      .then((data) => setApiData(data.slice(0, 10))) // Limitando a 10 elementos
       .catch((error) => console.error("Error fetching data:", error));
   },[]);
-  
+
   return (
     <div className="mt-20 mx-3 md:mt-28 lg:mx-28 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 justify-center">
       {apiData &&
@@ -25,17 +32,18 @@ const Cards=() => {
           <Card key={index} className="mt-6 w-96 shadow-none">
             <CardHeader color="blue-gray" className="relative h-56">
               <img
-                src={item.urlToImage}
+                //src={item.urlToImage}
+                src={item.thumbnailUrl}
                 alt="card-image"
                 style={{ objectFit: "cover", width: "100%", height: "100%" }}
               />
             </CardHeader>
             <CardBody className="rounded-xl text-black">
-              <Typography variant="h5" className="mb-2">
-                {item.author || 'Autor desconocido'}
-              </Typography>
+              {/*<Typography variant="h5" className="mb-2">
+                {item.author || "Autor desconocido"}
+              </Typography>*/}
               <Typography className=" font-medium lg:text-sm">
-                {item.title || 'Titulo desconocido'}
+                {item.title || "Titulo desconocido"}
               </Typography>
             </CardBody>
             <CardFooter className="pt-0">
